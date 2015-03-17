d65203f8f988a89ad31578f62a4e928efe585533
Steve McNiven-Scott <steve@sitefinitysteve.com>
2015-03-17 19:14:15 +0000

    Changes made by Steve McNiven-Scott (steve@sitefinitysteve.com).

--- Tasks/app/app.js
+++ Tasks/app/app.js
@@ -1,6 +1,6 @@
 var application = require("application");
 application.mainModule = "app/main-page";
 
-global.TELERIK_BAAS_KEY = "4I1Rz7lGDGsx51VB";
+global.TELERIK_BAAS_KEY = "<YOURKEYHERE>";
 
 application.start();
